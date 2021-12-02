7f517381915c0f57c250fcc5b45947edf9beac8d
preston-Glenn <preston.glenn@outlook.com>
2021-12-02 13:13:38 -0600

    added drink test

---
+++ make_drink.py
@@ -0,0 +1,25 @@
+
+SPRITE = 0
+ORANGE_JUICE = 1
+
+
+def make_drink_spritzer():
+
+    # Update status and push to AWS
+
+
+    # Make drink
+    GPIO.setup(SPRITE, GPIO.OUT)
+    GPIO.setup(ORANGE_JUICE, GPIO.OUT)
+
+    GPIO.output(SPRITE, True)
+    GPIO.output(ORANGE_JUICE, True)
+
+    time.sleep(4)      
+
+    GPIO.output(SPRITE, False)   
+    GPIO.output(ORANGE_JUICE, False)
+
+    # Update status and push to AWS
+
+    print("Finished Drink")
